b0193a521b22c93972725d4517f43b3e1cb68235
eugeniopch <eugeniopch@gmail.com>
2023-08-16 19:49:36 -0300

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-function App() {
-  //Aqui deberias agregar los estados y los handlers para los inputs
+import { useState } from 'react'
+import Form from './Form'
+import Card from './Card'
+
+
+let autoId = 0
+
+const App = () => {
+  const [datos, setDatos] = useState()
+ 
+function handleSubmit (datos) {
+    setDatos(datos)
+  }
 
   return (
-    <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
-    </div>
-  );
+    <div>
+      <h2>Formulario</h2>
+      <Form onSubmit={handleSubmit} />
+      {datos && (
+        <div className="submitted-data">
+          <Card data={datos}/>
+        </div>
+      )}
+      </div>
+  )
 }
 
-export default App;
+export default App
+
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,20 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+
+
+function Card(user) {
   return (
-    <div>
-      <h2>Esto es un componente</h2>
+    <div className= 'Card'>
+      <h3>Valores ingresados:</h3>
+      <ul>
+
+          <li className='card' key={user.id}>
+            <p>Nombre: {user.data.nombre}</p>
+            <p>Helado favorito: {user.data.helado}</p>
+          </li>
+
+      </ul>
     </div>
   );
 }
